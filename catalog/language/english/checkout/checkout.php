53bf1bed102cb6e2118fa11c842b5e2d13b02a1a
Jacob M. Saporito <jmsaporito@ucdavis.edu>
2013-08-22 11:22:58 -0700

    Fixed syntax error in the checkout language file.

--- catalog/language/english/checkout/checkout.php
+++ catalog/language/english/checkout/checkout.php
@@ -59,7 +59,7 @@ $_['entry_building']                 = 'Building:';
 $_['entry_zone']                     = 'Region / State:';
 $_['entry_newsletter']               = 'I wish to subscribe to the %s newsletter.';
 $_['entry_shipping'] 	             = 'My delivery and billing addresses are the same.';
-$_['entry_dafis']                    = 'DaFIS/KFS Account:'
+$_['entry_dafis']                    = 'DaFIS/KFS Account:';
 
 // Error
 $_['error_warning']                  = 'There was a problem while trying to process your order! If the problem persists please try selecting a different payment method or you can contact the store owner by <a href="%s">clicking here</a>.';
