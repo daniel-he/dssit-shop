3b7bda9db96d2ae39c947641acae6038f53416f8
Jacob M. Saporito <jmsaporito@ucdavis.edu>
2013-08-29 11:53:00 -0700

    Started implementing ModelSysaidSysaid.

--- catalog/model/sysaid/sysaid.php
+++ catalog/model/sysaid/sysaid.php
@@ -4,44 +4,19 @@ class ModelSysaidSysaid extends Model {
    * Not a model that interacts with the database.
    * Instead it deals with Sysaid.
    */
-	public function makeTicket($data) {
-	  $ticket_no = '';
+  public function makeTicket($data) {
+    $ticket_no = '';
 
-	  $sysaidApi = new SoapClient(SOAP_WSDL);
+    $sysaidApi = new SoapClient(SOAP_WSDL);
+    $sysaidSessId = $sysaidApi->login(SYSAID_ACCOUNT, "AD3\\japorito", "Abithiw2itb.Boafft.Lbyl!");
 
-	  if ($ticket_no) {
-	    return $ticket_no;
-	  } else {
-	    return FALSE;
-	  }
-	}
-	
-	public function editCountry($country_id, $data) {
-		$this->cache->delete('country');
-	}
-	
-	public function deleteCountry($country_id) {
-		$this->cache->delete('country');
-	}
-	
-	public function getCountry($country_id) {
-		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "country WHERE country_id = '" . (int)$country_id . "'");
-		
-		return $query->row;
-	}
-		
-	public function getCountries($data = array()) {
-		if ($data) {
-			$sql = "SELECT * FROM " . DB_PREFIX . "country";
-			
+    $sysaidApi->logout($sysaddSessId);
 
-		}	
-	}
-	
-	public function getTotalCountries() {
-      	$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "country");
-		
-		return $query->row['total'];
-	}	
+    if ($ticket_no) {
+      return $ticket_no;
+    } else {
+      return FALSE;
+    }
+  }	
 }
 ?>
