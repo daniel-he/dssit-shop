6a0c91a0eabeb051aa931b171e6cb5b2977c4822
Jacob M. Saporito <jmsaporito@ucdavis.edu>
2013-08-30 09:38:13 -0700

    More work done on model_sysaid_sysaid.

--- catalog/model/sysaid/sysaid.php
+++ catalog/model/sysaid/sysaid.php
@@ -1,15 +1,54 @@
 <?php
+class ApiSysaidObject {
+  public $CIId;
+  public $catalogNumber;
+  public $custDate1;
+  public $custDate2;
+  public $manufacturer;
+  public $model;
+  public $name;
+  public $notes;
+  public $supplierID;
+  public $version;
+}
+
+class ApiServiceRequest extends ApiSysaidObject {
+  
+}
+
 class ModelSysaidSysaid extends Model {
   /*
    * Not a model that interacts with the database.
    * Instead it deals with Sysaid.
    */
-  public function getTicket($ticket_no) {
+  private function login() {
     $sysaidApi = new SoapClient(SYSAID_WSDL);
     $params = array("accountId" = SYSAID_ACCOUNT, "userName" => "AD3\\user", "password" => "secret_pass");
-    $sysaidSessId = $sysaidApi->login($params);
+    $sessId = get_object_vars($sysaidApi->login($params));
+    $sessId = $sessId["return"];
 
-    $sysaidApi->logout($sysaddSessId);
+    return $sessId;
+  }
+
+  private function logout() {
+    $sysaidApi = new SoapClient(SYSAID_WSDL);
+
+    $params = array("sessionId" => $sessId);
+    try {
+      $sysaidApi->logout($params);
+      return TRUE;
+    } catch (Exception $e) {
+      return FALSE;
+    }
+  }
+
+  public function getTicket($ticket_no) {
+    $sysaidApi = new SoapClient(SYSAID_WSDL);
+    $sessId = $this->login();
+
+    $params = array("sessionId" => $sessId, "apiSysObj" => new ApiSysaidObject(), "id" = $ticket_no);
+
+    $this->logout();
 
     if ($ticket_no) {
       return $ticket_no;
