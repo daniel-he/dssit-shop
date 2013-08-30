cc88c06ac8590b873c5964f09cc49f622c51bf10
Jacob M. Saporito <jmsaporito@ucdavis.edu>
2013-08-30 12:40:42 -0700

    Got getTicket() working.

--- catalog/model/sysaid/sysaid.php
+++ catalog/model/sysaid/sysaid.php
@@ -1,19 +1,82 @@
 <?php
 class ApiSysaidObject {
-  public $CIId;
-  public $catalogNumber;
-  public $custDate1;
-  public $custDate2;
-  public $manufacturer;
-  public $model;
-  public $name;
-  public $notes;
-  public $supplierID;
-  public $version;
 }
 
 class ApiServiceRequest extends ApiSysaidObject {
+  public $agreement;
+  public $archive;
+  public $assignCounter;
+  public $assignedGroup;
+  public $assignedTo;
+  public $CIId;
+  public $category;
+  public $cc;
+  public $changeCategory;
+  public $closeTime;
+  public $closureInformation;
+  public $computerID;
+  public $currentSupportLevel;
+  public $custDate1;
+  public $custDate2;
+  public $custInt1;
+  public $custInt2;
+  public $custList1;
+  public $custList2;
+  public $custNotes;
+  public $custText1;
+  public $custText2;
+  public $customDateFields;
+  public $customFields;
+  public $description;
+  public $dueDate;
+  public $emailAccount;
+  public $escalation;
+  public $followupActualDate;
+  public $followupPlannedDate;
+  public $followupText;
+  public $followupUser;
+  public $id;
+  public $impact;
+  public $insertTime;
+  public $location;
+  public $maxSupportLevel;
+  public $notes;
+  public $parentLink;
+  public $priority;
+  public $projectID;
+  public $reopenCounter;
+  public $requestUser;
+  public $resolution;
+  public $responsibleManager;
+  public $solution;
+  public $source;
+  public $srSubType;
+  public $srType;
+  public $status;
+  public $subCategory;
+  public $submitUser;
+  public $successRating;
+  public $taskID;
+  public $thirdLevelCategory;
+  public $title;
+  public $updateTime;
+  public $updateUser;
+  public $urgency;
+  public $userManager;
+  public $version;
+  public $workaround;
+
+  public function ApiServiceRequest() {
+    $this->customDateFields = new stdClass();
+    $this->customDateFields->entry = new stdClass();
+    $this->customDateFields->entry->key = new stdClass();
+    $this->customDateFields->entry->value = new stdClass();
   
+    $this->customFields = new stdClass();
+    $this->customFields->entry = new stdClass();
+    $this->customFields->entry->key = new stdClass();
+    $this->customFields->entry->value = new stdClass();
+  }
 }
 
 class ModelSysaidSysaid extends Model {
@@ -21,37 +84,37 @@ class ModelSysaidSysaid extends Model {
    * Not a model that interacts with the database.
    * Instead it deals with Sysaid.
    */
-  private function login() {
-    $sysaidApi = new SoapClient(SYSAID_WSDL);
-    $params = array("accountId" = SYSAID_ACCOUNT, "userName" => "AD3\\user", "password" => "secret_pass");
+  protected function login() {
+    $sysaidApi = new SoapClient(SYSAID_WSDL, array('classmap' => array('apiServiceRequest' => 'ApiServiceRequest')));
+
+    $params = array("accountId" => SYSAID_ACCOUNT, "userName" => "your_user", "password" => "your_pass");
+
     $sessId = get_object_vars($sysaidApi->login($params));
     $sessId = $sessId["return"];
 
     return $sessId;
   }
 
-  private function logout() {
+  protected function logout($sessId) {
     $sysaidApi = new SoapClient(SYSAID_WSDL);
-
+	
     $params = array("sessionId" => $sessId);
-    try {
-      $sysaidApi->logout($params);
-      return TRUE;
-    } catch (Exception $e) {
-      return FALSE;
-    }
+    $sysaidApi->logout($params);
   }
 
   public function getTicket($ticket_no) {
-    $sysaidApi = new SoapClient(SYSAID_WSDL);
-    $sessId = $this->login();
+    $sessId = $this->login(); 
 
-    $params = array("sessionId" => $sessId, "apiSysObj" => new ApiSysaidObject(), "id" = $ticket_no);
+    $sysaidApi = new SoapClient(SYSAID_WSDL, array('classmap' => array('apiServiceRequest' => 'ApiServiceRequest'));
 
-    $this->logout();
+    $params = array("sessionId" => $sessId, "apiSysObj" => new ApiServiceRequest(), "id" => $ticket_no);
+    $ticket = get_object_vars($sysaidApi->loadByStringId($params));
+    $ticket = $ticket["return"];
 
-    if ($ticket_no) {
-      return $ticket_no;
+    $this->logout($sessId);
+
+    if ($ticket) {
+      return $ticket;
     } else {
       return FALSE;
     }
