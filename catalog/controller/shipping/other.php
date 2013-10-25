2225c3b61b150e71112d33cbdb95e3f289758bd1
Jacob Saporito <jmsaporito@ucdavis.edu>
2013-10-25 10:34:39 -0700

    Initial catalog-side commit for adding 'other' shipping option.

---
+++ catalog/controller/shipping/other.php
@@ -0,0 +1,47 @@
+<?php 
+class ControllerShippingServiceCenterPickup extends Controller {
+  	public function index() {	
+		$this->language->load('checkout/checkout');
+		
+		$this->data['text_select'] = $this->language->get('text_select');
+		$this->data['text_none'] = $this->language->get('text_none');
+
+		$this->data['button_continue'] = $this->language->get('button_continue');
+		$this->data['entry_additional_info'] = $this->language->get('entry_additional_info');
+					
+		if (isset($this->session->data['shippingInfo']['additional_info'])) {
+			$this->data['additional_info'] = $this->session->data['shippingInfo']['additional_info'];
+		} else {
+		  $this->data['additional_info'] = '';
+		}
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/shipping/other.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/shipping/other.tpl';
+		} else {
+			$this->template = 'default/template/shipping/other.tpl';
+		}		
+		
+		$this->response->setOutput($this->render());
+	}
+	
+	public function validate() {
+		$json = array();
+		
+		// Validate if customer is logged in.
+		if (!$this->customer->isLogged()) {
+			$json['redirect'] = $this->url->link('checkout/checkout', '', 'SSL');
+		} 			
+		
+		// Validate cart has products and has stock.
+		if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+			$json['redirect'] = $this->url->link('checkout/cart');		
+		} 
+		
+		if (!$json) {
+			$this->session->data['shippingInfo']['additional_info'] = trim($this->request->post['additional_info']);
+		}
+		
+		$this->response->setOutput(json_encode($json));		
+	}
+}
+?>
---
+++ catalog/view/theme/default/template/shipping/other.tpl
@@ -0,0 +1,5 @@
+<textarea name="additional_info" rows="8" style="width: 98%;"><?php echo $additional_info; ?></textarea>
+<br />
+<div class="buttons">
+  <div class="right"><input type="button" value="<?php echo $button_continue; ?>" id="button-shipping-address" class="button" /></div>
+</div>
