c9c2fed7f1fe908d70f5ff8d20fa0cba3aab46b7
Jacob Saporito <jmsaporito@ucdavis.edu>
2013-10-31 12:17:12 -0700

    Admin side stuff, and added permissions for sysaid/config.

--- admin/controller/sysaid/config.php
+++ admin/controller/sysaid/config.php
@@ -1,39 +1,32 @@
 <?php
-class ControllerModuleAffiliate extends Controller {
+class ControllerSysaidTicket extends Controller {
 	private $error = array(); 
 	
 	public function index() {   
-		$this->language->load('module/affiliate');
+		$this->language->load('sysaid/config');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 		
 		$this->load->model('setting/setting');
 				
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('affiliate', $this->request->post);		
+		  //$this->model_setting_setting->editSetting('sysaid_config', array("sysaid_config_service_centers" => array_values($this->request->post), "sysaid_config_buildings" => array_values($this->request->post)));	
+		  var_dump($this->request->post);	
 					
 			$this->session->data['success'] = $this->language->get('text_success');
-						
+			
 			$this->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
 		}
 				
 		$this->data['heading_title'] = $this->language->get('heading_title');
-
-		$this->data['text_enabled'] = $this->language->get('text_enabled');
-		$this->data['text_disabled'] = $this->language->get('text_disabled');
-		$this->data['text_content_top'] = $this->language->get('text_content_top');
-		$this->data['text_content_bottom'] = $this->language->get('text_content_bottom');		
-		$this->data['text_column_left'] = $this->language->get('text_column_left');
-		$this->data['text_column_right'] = $this->language->get('text_column_right');
 		
-		$this->data['entry_layout'] = $this->language->get('entry_layout');
-		$this->data['entry_position'] = $this->language->get('entry_position');
-		$this->data['entry_status'] = $this->language->get('entry_status');
-		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
+		$this->data['entry_building'] = $this->language->get('entry_building');
+		$this->data['entry_service_center'] = $this->language->get('entry_service_center');
 		
 		$this->data['button_save'] = $this->language->get('button_save');
 		$this->data['button_cancel'] = $this->language->get('button_cancel');
-		$this->data['button_add_module'] = $this->language->get('button_add_module');
+		$this->data['button_add_service_center'] = $this->language->get('button_add_service_center');
+		$this->data['button_add_building'] = $this->language->get('button_add_building');
 		$this->data['button_remove'] = $this->language->get('button_remove');
 		
  		if (isset($this->error['warning'])) {
@@ -58,27 +51,31 @@ class ControllerModuleAffiliate extends Controller {
 		
    		$this->data['breadcrumbs'][] = array(
        		'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('module/affiliate', 'token=' . $this->session->data['token'], 'SSL'),
+			'href'      => $this->url->link('sysaid/config', 'token=' . $this->session->data['token'], 'SSL'),
       		'separator' => ' :: '
    		);
 		
-		$this->data['action'] = $this->url->link('module/affiliate', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['action'] = $this->url->link('sysaid/config', 'token=' . $this->session->data['token'], 'SSL');
 		
 		$this->data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
 		
-		$this->data['modules'] = array();
+		$this->data['service_centers'] = array();
 		
-		if (isset($this->request->post['affiliate_module'])) {
-			$this->data['modules'] = $this->request->post['affiliate_module'];
-		} elseif ($this->config->get('affiliate_module')) { 
-			$this->data['modules'] = $this->config->get('affiliate_module');
-		}	
-					
-		$this->load->model('design/layout');
+		if ($this->request->server['REQUEST_METHOD'] == 'POST') {
+		  $this->data['service_centers'] = array_values($this->request->post);
+		} elseif ($this->config->get('sysaid_config_service_centers')) { 
+			$this->data['service_centers'] = $this->config->get('sysaid_config_service_centers');
+		}
 		
-		$this->data['layouts'] = $this->model_design_layout->getLayouts();
+		$this->data['buildings'] = array();
 		
-		$this->template = 'module/affiliate.tpl';
+		if ($this->request->server['REQUEST_METHOD'] == 'POST') {
+		  $this->data['buildings'] = array_values($this->request->post);
+		} elseif ($this->config->get('sysaid_config_buildings')) { 
+			$this->data['buildings'] = $this->config->get('sysaid_config_buildings');
+		}
+		
+		$this->template = 'sysaid/config.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
@@ -88,7 +85,7 @@ class ControllerModuleAffiliate extends Controller {
 	}
 	
 	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'module/affiliate')) {
+		if (!$this->user->hasPermission('modify', 'sysaid/config')) {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 		
