04ec495f39acf7422702205b06f664f2125d52fe
Jacob Saporito <jmsaporito@ucdavis.edu>
2013-10-31 12:27:29 -0700

    Actually added the new language file.

---
+++ admin/language/english/sysaid/config.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']       = 'Service Center and Building Configuration for Sysaid';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified sysaid service center and building configuration!';
+
+// Entry
+$_['entry_service_center']= 'Service Centers:';
+$_['entry_building']      = 'Buildings:';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify sysaid configuration!';
+?>
