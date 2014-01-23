772a6cbad06df110f0c12bb2671987f8ea1ae585
Jacob Saporito <jmsaporito@ucdavis.edu>
2014-01-23 09:11:18 -0800

    Initial code dump to add shipping method to the admin-side order form. With files added.

---
+++ admin/model/shipping/office_delivery.php
@@ -0,0 +1,29 @@
+<?php
+class ModelShippingOfficeDelivery extends Model {
+  function getQuote($address) {
+    $this->language->load('shipping/office_delivery');
+    
+    $method_data = array();
+    
+    $quote_data = array();
+    
+    $quote_data['office_delivery'] = array(
+      'code'         => 'office_delivery.office_delivery',
+      'title'        => $this->language->get('text_description'),
+      'cost'         => $this->config->get('office_delivery_cost'),
+      'tax_class_id' => $this->config->get('office_delivery_tax_class_id'),
+      'text'         => $this->currency->format(0)
+    );
+
+    $method_data = array(
+      'code'       => 'office_delivery',
+      'title'      => $this->language->get('text_title'),
+      'quote'      => $quote_data,
+      'sort_order' => $this->config->get('office_delivery_sort_order'),
+      'error'      => false
+    );
+	
+    return $method_data;
+  }
+}
+?>
---
+++ admin/model/shipping/other.php
@@ -0,0 +1,29 @@
+<?php
+class ModelShippingOther extends Model {
+  function getQuote($address) {
+    $this->language->load('shipping/other');
+    
+    $method_data = array();
+    
+    $quote_data = array();
+    
+    $quote_data['other'] = array(
+      'code'         => 'other.other',
+      'title'        => $this->language->get('text_description'),
+      'cost'         => $this->config->get('other_cost'),
+      'tax_class_id' => $this->config->get('other_tax_class_id'),
+      'text'         => $this->currency->format(0)
+    );
+
+    $method_data = array(
+      'code'       => 'other',
+      'title'      => $this->language->get('text_title'),
+      'quote'      => $quote_data,
+      'sort_order' => $this->config->get('other_sort_order'),
+      'error'      => false
+    );
+	
+    return $method_data;
+  }
+}
+?>
